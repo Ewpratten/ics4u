52abc70124884dbafbabfbc1c15f88357156072d
Evan Pratten <ewpratten@gmail.com>
2019-10-04 19:26:36 -0400

    binhex2

--- projects/binaryhex/src/main/java/binaryhex/App.java
+++ projects/binaryhex/src/main/java/binaryhex/App.java
@@ -79,12 +79,56 @@ public class App {
 
     }
 
-    static void DtoB() {
-        int decimal = Integer.parseInt(JOptionPane.showInputDialog(null, "Please enter a binary number",
-                "Decimal to Binary", JOptionPane.INFORMATION_MESSAGE));
+    /**
+     * Convert a u_int8 to a binary string
+     * 
+     * Bigger numbers are not allowed in order to reduce processing times (and this
+     * is only for demonstration)
+     * 
+     * @param x 8-bit int
+     * @return Binary string
+     */
+    static String toBin(int x) {
+        // Handle the number 0
+        if (x == 0) {
+            return "0";
+        }
 
-        // Convert file
-        String binary = Integer.toBinaryString(decimal);
+        // Create holder for binary. 8 bit max
+        char binary[] = new char[8];
+
+        int index = 0;
+        while (x > 0) {
+            binary[index++] = (char) ('0' + (x % 2));
+            x = x / 2;
+        }
+
+        return new String(binary);
+
+    }
+
+    static void DtoB() {
+        String input = JOptionPane.showInputDialog(null, "Please enter a decimal number (max 255)", "Decimal to Binary",
+                JOptionPane.INFORMATION_MESSAGE);
+
+        // Handle null inputs
+        if (input == null) {
+            return;
+        }
+
+        // Parse to int
+        int decimal = Integer.parseInt(input);
+
+        // Handle incorrect bounds
+        if (decimal < 0 || decimal > 255) {
+            JOptionPane.showMessageDialog(null,
+                    "Your number is not between 0 and 255. This program only accepts 8-bit ints in order to keep the processing time low.",
+                    "Binary conversion failed", JOptionPane.INFORMATION_MESSAGE);
+            return;
+        }
+
+        // Convert to bin
+        String binary = toBin(decimal);
 
         JOptionPane.showMessageDialog(null, decimal + " converted to binary is " + binary, "Binary conversion complete",
                 JOptionPane.INFORMATION_MESSAGE);
