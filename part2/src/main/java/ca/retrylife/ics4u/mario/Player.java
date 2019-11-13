ad43fd8cd3d26e02813dba34a6d93aae889414f2
Evan Pratten <ewpratten@gmail.com>
2019-11-12 19:41:33 -0500

    Added collision prediction

--- part2/src/main/java/ca/retrylife/ics4u/mario/Player.java
+++ part2/src/main/java/ca/retrylife/ics4u/mario/Player.java
@@ -1,40 +1,107 @@
 package ca.retrylife.ics4u.mario;
 
 import ca.retrylife.libics.graphics.Window;
+import ca.retrylife.libics.math.MathUtils;
 
 import java.awt.Point;
 import java.awt.Dimension;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
 
 public class Player {
 
+    /* Config */
+    private final double max_speed = 10;
+    private double speed = 100;
+
+    /* Locals */
     Window window;
     Point position;
     Dimension size;
+    private double dx, dy = 0.0;
 
     public Player(Point pos, Dimension size) {
         // Set player pos
         position = pos;
         this.size = size;
 
-
         // Build a window to contain player sprite
         window = new Window("Mario", pos, size);
         window.registerUpdateHandler(this::update);
-    }
 
-    private void update(Window win) {
-        System.out.println(isCollidingWithWorld());
+        // Set onClose callback
+        window.onClose(() -> {
+            System.exit(0);
+        });
+
+        // Set Keystroke listeners
+        window.registerKBDCallback(65, this::handleLeftMOV); // A
+        window.registerKBDCallback(68, this::handleRightMOV); // D
     }
 
     public void start() {
         window.start();
     }
 
-    private boolean isCollidingWithWorld(){
-        boolean leftTop = World.getInstance().isColliding(position);
-        boolean leftBottom = World.getInstance().isColliding(position);
-        boolean rightTop = World.getInstance().isColliding(position);
-        boolean rightBottom = World.getInstance().isColliding(position);
+    private void update(Window win) {
+
+    }
+
+    /**
+     * To be called independently of the main update loop.. Once per keystroke
+     */
+    private void updatePos() {
+        // Move window from velocities
+        position.x += dx;
+        position.y += dy;
+        window.setLocation(position);
+        dx = 0;
+    }
+
+    private void handleLeftMOV() {
+
+        // Only allow movement if not about to collide
+        if (predictCollision(new Point((int) (position.x - max_speed), position.y))) {
+            dx = 0;
+            System.out.println("WALL!");
+            return;
+        }
+
+        dx = MathUtils.clamp(-speed, -max_speed, max_speed);
+
+        updatePos();
+    }
+
+    private void handleRightMOV() {
+
+        // Only allow movement if not about to collide
+        if (predictCollision(new Point((int) (position.x + max_speed + size.getWidth()), position.y))) {
+            dx = 0;
+            System.out.println("WALL!");
+            return;
+        }
+
+        dx = MathUtils.clamp(speed, -max_speed, max_speed);
+
+        updatePos();
+    }
+
+    /**
+     * Determine if the player is colliding with the world
+     * 
+     * @return Is colliding?
+     */
+    public boolean isCollidingWithWorld() {
+
+        // If we predict our current location, Things work
+        return predictCollision(position);
+    }
+
+    private boolean predictCollision(Point pos) {
+        boolean leftTop = World.getInstance().isColliding(pos);
+        boolean leftBottom = World.getInstance().isColliding(pos);
+        boolean rightTop = World.getInstance().isColliding(pos);
+        boolean rightBottom = World.getInstance().isColliding(pos);
 
         return leftTop || leftBottom || rightTop || rightBottom;
     }
--- part2/src/main/java/ca/retrylife/libics/graphics/Window.java
+++ part2/src/main/java/ca/retrylife/libics/graphics/Window.java
@@ -1,39 +1,90 @@
 package ca.retrylife.libics.graphics;
 
 import javax.swing.JFrame;
+import javax.swing.KeyStroke;
+
 import java.awt.Point;
+import java.util.HashMap;
 import java.util.function.Consumer;
 import java.awt.Dimension;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
 
+/**
+ * Wrapper and utils for a JFrame window
+ */
 public class Window extends JFrame {
     private static final long serialVersionUID = 2406077714629079044L;
 
     private Consumer<Window> drawCallback = null;
     private Thread thread;
+    private HashMap<Integer, Runnable> keybindings = new HashMap<Integer, Runnable>();
 
+    /**
+     * Create a new Window, and display it
+     * 
+     * @param name Window title
+     * @param pos  Window position
+     * @param size Window size
+     */
     public Window(String name, Point pos, Dimension size) {
         super(name);
 
+        // Config window
         this.setSize(size);
         this.setLocation(pos);
         this.setVisible(true);
 
+        // Create a thread for drawing callbacks
         this.thread = new Thread(this::run);
+
+        // Enable keystroke callbacks
+        this.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                // System.out.println("Key pressed code=" + e.getKeyCode() + ", char=" +
+                // e.getKeyChar());
+                handleKeystroke(e);
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+            }
+        });
 
     }
 
+    /**
+     * Register a callback method for each window frame
+     * 
+     * @param callback Callback method
+     */
     public void registerUpdateHandler(Consumer<Window> callback) {
         drawCallback = callback;
     }
 
+    /**
+     * Start drawing thread
+     */
     public void start() {
         thread.start();
     }
 
+    /**
+     * Stop drawing thread
+     */
     public void stop() {
         thread.interrupt();
     }
 
+    /**
+     * Run in thread, handles callbacks
+     */
     private void run() {
 
         while (true) {
@@ -48,6 +99,74 @@ public class Window extends JFrame {
             drawCallback.accept(this);
         }
 
+    }
+
+    /**
+     * Register a keystroke callback for a specific char
+     * 
+     * @param key      Char to watch for
+     * @param callback Method to run on keypress (non-latching)
+     */
+    public void registerKBDCallback(char key, Runnable callback) {
+        KeyStroke ks = KeyStroke.getKeyStroke(key, 0);
+        registerKBDCallback(ks.getKeyCode(), callback);
+    }
+
+    /**
+     * Register a keystroke callback for a specific key code
+     * 
+     * @param key      Key code to watch for
+     * @param callback Method to run on keypress (non-latching)
+     */
+    public void registerKBDCallback(int keycode, Runnable callback) {
+        keybindings.put(keycode, callback);
+    }
+
+    /**
+     * Remove a keyboard event callback
+     * 
+     * @param key Key char to remove
+     */
+    public void unregisterKBDCallback(char key) {
+        KeyStroke ks = KeyStroke.getKeyStroke(key, 0);
+        keybindings.remove(ks.getKeyCode());
+    }
+
+    /**
+     * Remove a keyboard event callback
+     * 
+     * @param key Key code to remove
+     */
+    public void unregisterKBDCallback(int keycode) {
+        keybindings.remove(keycode);
+    }
+
+    private void handleKeystroke(KeyEvent ke) {
+
+        // Run the callback if it exists
+        if (keybindings.containsKey(ke.getKeyCode())) {
+            // Run the callback
+            keybindings.get(ke.getKeyCode()).run();
+        }
+
+    }
+
+    /**
+     * Set an action on window close
+     * 
+     * @param run Event runnable
+     */
+    public void onClose(Runnable run) {
+
+        // Register the JFrame callback
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            @Override
+            public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+
+                // Run the runnable
+                run.run();
+            }
+        });
     }
 
 }
