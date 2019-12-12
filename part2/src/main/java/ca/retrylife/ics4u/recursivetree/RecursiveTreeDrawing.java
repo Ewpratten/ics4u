fe80ee50b6b077c431ebd6b0e4602bdb3566c04d
Evan Pratten <ewpratten@gmail.com>
2019-12-12 15:41:26 -0500

    format

--- part2/src/main/java/ca/retrylife/ics4u/recursivetree/RecursiveTreeDrawing.java
+++ part2/src/main/java/ca/retrylife/ics4u/recursivetree/RecursiveTreeDrawing.java
@@ -20,45 +20,34 @@ import javax.swing.SwingUtilities;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-public class RecursiveTreeDrawing
-{
-    public static void main(String[] args) 
-    {
-        SwingUtilities.invokeLater(new Runnable()
-        {
+public class RecursiveTreeDrawing {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
             @Override
-            public void run()
-            {
+            public void run() {
                 createAndShowGUI();
             }
         });
-    }    
+    }
 
-    private static void createAndShowGUI()
-    {
+    private static void createAndShowGUI() {
         JFrame f = new JFrame();
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.getContentPane().setLayout(new BorderLayout());
 
         RecursiveTreeDrawingPanel p = new RecursiveTreeDrawingPanel();
-        p.setPreferredSize(new Dimension(500,500));
+        p.setPreferredSize(new Dimension(500, 500));
         f.getContentPane().add(p, BorderLayout.CENTER);
 
-        JPanel c = new JPanel(new GridLayout(0,1));
+        JPanel c = new JPanel(new GridLayout(0, 1));
 
-        c.add(createControl("left length", 0, 0.9, 
-            d -> p.setLeftLengthFactor(d)));
-        c.add(createControl("left width", 0, 0.9, 
-            d -> p.setLeftWidthFactor(d)));
-        c.add(createControl("left angle", 0, Math.PI, 
-            d -> p.setLeftAngleDelta(d)));
+        c.add(createControl("left length", 0, 0.9, d -> p.setLeftLengthFactor(d)));
+        c.add(createControl("left width", 0, 0.9, d -> p.setLeftWidthFactor(d)));
+        c.add(createControl("left angle", 0, Math.PI, d -> p.setLeftAngleDelta(d)));
 
-        c.add(createControl("right length", 0, 0.9, 
-            d -> p.setRightLengthFactor(d)));
-        c.add(createControl("right width", 0, 0.9, 
-            d -> p.setRightWidthFactor(d)));
-        c.add(createControl("right angle", -Math.PI, 0, 
-            d -> p.setRightAngleDelta(d)));
+        c.add(createControl("right length", 0, 0.9, d -> p.setRightLengthFactor(d)));
+        c.add(createControl("right width", 0, 0.9, d -> p.setRightWidthFactor(d)));
+        c.add(createControl("right angle", -Math.PI, 0, d -> p.setRightAngleDelta(d)));
         f.getContentPane().add(c, BorderLayout.SOUTH);
 
         f.pack();
@@ -66,18 +55,14 @@ public class RecursiveTreeDrawing
         f.setVisible(true);
     }
 
-    private static JPanel createControl(
-        String name, double min, double max, DoubleConsumer doubleConsumer)
-    {
-        JPanel p = new JPanel(new GridLayout(1,0));
+    private static JPanel createControl(String name, double min, double max, DoubleConsumer doubleConsumer) {
+        JPanel p = new JPanel(new GridLayout(1, 0));
         p.add(new JLabel(name));
         JSlider slider = new JSlider(0, 100, 0);
-        slider.addChangeListener(new ChangeListener()
-        {
+        slider.addChangeListener(new ChangeListener() {
 
             @Override
-            public void stateChanged(ChangeEvent e)
-            {
+            public void stateChanged(ChangeEvent e) {
                 int value = slider.getValue();
                 double v = value / 100.0;
                 double d = min + v * (max - min);
@@ -91,85 +76,64 @@ public class RecursiveTreeDrawing
 
 }
 
-
-class RecursiveTreeDrawingPanel extends JPanel
-{
+class RecursiveTreeDrawingPanel extends JPanel {
     private double leftLengthFactor = 3.0 / 4.0;
     private double leftWidthFactor = 3.0 / 4.0;
     private double leftAngleDelta = Math.PI / 5.0;
     private double rightLengthFactor = 2.0 / 3.0;
     private double rightWidthFactor = 1.0 / 2.0;
-    private double rightAngleDelta = - Math.PI / 5.0; 
+    private double rightAngleDelta = -Math.PI / 5.0;
 
     @Override
-    protected void paintComponent(Graphics gr)
-    {
+    protected void paintComponent(Graphics gr) {
         super.paintComponent(gr);
-        Graphics2D g = (Graphics2D)gr;
+        Graphics2D g = (Graphics2D) gr;
         g.setColor(Color.BLACK);
-        g.fillRect(0,0,getWidth(),getHeight());
-        g.setRenderingHint(
-            RenderingHints.KEY_ANTIALIASING, 
-            RenderingHints.VALUE_ANTIALIAS_ON);
-        Point2D start = new Point2D.Double(
-            getWidth() * 0.5, 
-            getHeight() * 0.7);
+        g.fillRect(0, 0, getWidth(), getHeight());
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        Point2D start = new Point2D.Double(getWidth() * 0.5, getHeight() * 0.7);
         g.setColor(Color.GRAY);
         drawTree(g, start, 100.0, 2.0, 10.0, 0);
     }
 
-    private void drawTree(Graphics2D g, 
-        Point2D start, double length, double minLength, 
-        double width, double alpha)
-    {
-        if (length < minLength)
-        {
+    private void drawTree(Graphics2D g, Point2D start, double length, double minLength, double width, double alpha) {
+        if (length < minLength) {
             return;
         }
-        g.setStroke(new BasicStroke((float)width, 
-            BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
-        Point2D end = new Point2D.Double(
-            start.getX() + Math.sin(alpha + Math.PI) * length,
-            start.getY() + Math.cos(alpha + Math.PI) * length);
+        g.setStroke(new BasicStroke((float) width, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+        Point2D end = new Point2D.Double(start.getX() + Math.sin(alpha + Math.PI) * length,
+                start.getY() + Math.cos(alpha + Math.PI) * length);
         g.draw(new Line2D.Double(start, end));
-        drawTree(g, end, length * leftLengthFactor, minLength, 
-            width * leftWidthFactor, alpha + leftAngleDelta);
-        drawTree(g, end, length * rightLengthFactor, minLength, 
-            width * rightWidthFactor, alpha + rightAngleDelta);
+        drawTree(g, end, length * leftLengthFactor, minLength, width * leftWidthFactor, alpha + leftAngleDelta);
+        drawTree(g, end, length * rightLengthFactor, minLength, width * rightWidthFactor, alpha + rightAngleDelta);
     }
 
-    public void setLeftLengthFactor(double leftLengthFactor)
-    {
+    public void setLeftLengthFactor(double leftLengthFactor) {
         this.leftLengthFactor = leftLengthFactor;
         repaint();
     }
 
-    public void setLeftWidthFactor(double leftWidthFactor)
-    {
+    public void setLeftWidthFactor(double leftWidthFactor) {
         this.leftWidthFactor = leftWidthFactor;
         repaint();
     }
 
-    public void setLeftAngleDelta(double leftAngleDelta)
-    {
+    public void setLeftAngleDelta(double leftAngleDelta) {
         this.leftAngleDelta = leftAngleDelta;
         repaint();
     }
 
-    public void setRightLengthFactor(double rightLengthFactor)
-    {
+    public void setRightLengthFactor(double rightLengthFactor) {
         this.rightLengthFactor = rightLengthFactor;
         repaint();
     }
 
-    public void setRightWidthFactor(double rightWidthFactor)
-    {
+    public void setRightWidthFactor(double rightWidthFactor) {
         this.rightWidthFactor = rightWidthFactor;
         repaint();
     }
 
-    public void setRightAngleDelta(double rightAngleDelta)
-    {
+    public void setRightAngleDelta(double rightAngleDelta) {
         this.rightAngleDelta = rightAngleDelta;
         repaint();
     }
