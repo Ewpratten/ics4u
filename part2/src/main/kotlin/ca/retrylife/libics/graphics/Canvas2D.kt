01309b1c028e54e66deb5bda6c5d4dc8b6bb962c
Evan Pratten <ewpratten@gmail.com>
2019-12-13 12:37:53 -0500

    I didnt a thing

---
+++ part2/src/main/kotlin/ca/retrylife/libics/graphics/Canvas2D.kt
@@ -0,0 +1,11 @@
+package ca.retrylife.libics.graphics;
+
+import java.awt.Graphics;
+import java.util.function.Consumer;
+
+import javax.swing.JPanel;
+import java.awt.Graphics2D;
+
+class Canvas2D(val callback: Consumer) extends JPanel{
+
+}
